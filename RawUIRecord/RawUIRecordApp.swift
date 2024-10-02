1c3fac6593fabb17092ab2fb8d692ce674f7a250
Minh Phu Pham <phamminhphueur@gmail.com>
2024-10-02 17:00:06 +0700

    Initial Commit

---
+++ RawUIRecord/RawUIRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawUIRecordApp.swift
+//  RawUIRecord
+//
+//  Created by Phu Pham on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RawUIRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RawUIRecordTests/RawUIRecordTests.swift
@@ -0,0 +1,16 @@
+//
+//  RawUIRecordTests.swift
+//  RawUIRecordTests
+//
+//  Created by Phu Pham on 2/10/24.
+//
+
+import Testing
+
+struct RawUIRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
