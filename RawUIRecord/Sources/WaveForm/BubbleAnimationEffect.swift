f49ccad1554854768563d12f4258ec1e5f498b8a
Minh Phu Pham <phamminhphueur@gmail.com>
2024-10-24 11:44:09 +0700

    Let allow to make config to edit WaveForm

--- RawUIRecord/Sources/WaveForm/BubbleAnimationEffect.swift
+++ RawUIRecord/Sources/WaveForm/BubbleAnimationEffect.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 
+struct BubbleConfigProps {
+    var bubbleCount: Int = 10
+    var bubbleSize: CGFloat = 10
+    var bubbleColor: Color = .white
+    var waveRollsHeight: CGFloat = 20
+    var lifetimeRange: (min: Double, max: Double) = (min: 1.6, max: 2.2)
+}
+
 struct BubbleEffectView: View {
-    @StateObject var viewModel: BubbleEffectViewModel = BubbleEffectViewModel()
+    @StateObject private var viewModel: BubbleEffectViewModel
+    
+    init(config: BubbleConfigProps = .init()) {
+        _viewModel = StateObject(wrappedValue: BubbleEffectViewModel(config: config))
+    }
     
     var body: some View {
-        ZStack{
+        ZStack {
             // Show bubble views for each bubble
             ForEach(viewModel.bubbles) { bubble in
                 BubbleView(bubble: bubble)
@@ -34,46 +46,54 @@ struct BubbleEffectView: View {
         }
     }
 }
+
 class BubbleEffectViewModel: ObservableObject {
     @Published var bubbles: [BubbleViewModel] = []
     private var frameSize: CGSize = .zero
     private var timer: Timer?
-    private var timerCount: Int = 0
-    private var bubbleCount: Int = 10
-    private var waveRollsHeight: CGFloat = 20
-    private let lifetimeRange: (min: Double, max: Double) = (min: 1.6, max: 2.2)
-    
-    func onFrameSizeChange(_ size: CGSize){
+    private var bubbleConfig: BubbleConfigProps
+
+    init(config: BubbleConfigProps) {
+        self.bubbleConfig = config
+    }
+
+    func onFrameSizeChange(_ size: CGSize) {
         if self.frameSize != size {
             self.frameSize = size
             self.addBubbles()
         }
     }
     
-    func addBubbles(){
-        if timer != nil{
-            timer?.invalidate()
-        }
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {[weak self] (timer) in
+    func addBubbles() {
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             guard let self = self else {
                 self?.clearTimer()
                 return
             }
             
-            let yRoadHeight: CGFloat = max(0, self.frameSize.height - waveRollsHeight)
+            let yRoadHeight: CGFloat = max(0, self.frameSize.height - self.bubbleConfig.waveRollsHeight)
             let randomX: CGFloat = .random(in: (self.frameSize.width * 1/3)...(self.frameSize.width * 2/3))
-            let randomLifeTime: TimeInterval = .random(in: self.lifetimeRange.min...self.lifetimeRange.max)
-            let bubble = BubbleViewModel(height: 10, width: 10, x: randomX, y: yRoadHeight, color: .white, lifetime: randomLifeTime)
+            let randomLifetime: TimeInterval = .random(in: self.bubbleConfig.lifetimeRange.min...self.bubbleConfig.lifetimeRange.max)
+
+            let bubble = BubbleViewModel(
+                height: self.bubbleConfig.bubbleSize,
+                width: self.bubbleConfig.bubbleSize,
+                x: randomX,
+                y: yRoadHeight,
+                color: self.bubbleConfig.bubbleColor,
+                heightLitmited: self.bubbleConfig.waveRollsHeight,
+                lifetime: randomLifetime
+            )
             
-            //Add to array
+            // Add to array
             self.bubbles.append(bubble)
             
-            //Get rid if the bubble at the end of its lifetime
-            Timer.scheduledTimer(withTimeInterval: bubble.lifetime, repeats: false, block: {[weak self] _ in
-                self?.bubbles.removeAll(where: {
-                    $0.id == bubble.id
-                })
-            })
+            // Get rid of the bubble at the end of its lifetime
+            Timer.scheduledTimer(withTimeInterval: bubble.lifetime, repeats: false) { [weak self] _ in
+                self?.bubbles.removeAll { $0.id == bubble.id }
+            }
         }
     }
     
@@ -82,6 +102,7 @@ class BubbleEffectViewModel: ObservableObject {
         timer = nil
     }
 }
+
 
 struct BubbleView: View {
     //If you want to change the bubble's variables you need to observe it
@@ -97,7 +118,7 @@ struct BubbleView: View {
                 
                 withAnimation(.linear(duration: bubble.lifetime)){
                     //Go up
-                    self.bubble.y = -bubble.height
+                    self.bubble.y = -(bubble.height - bubble.heightLitmited)
                     //Go sideways
 //                    self.bubble.x += bubble.xFinalValue()
                     //Change size
@@ -124,13 +145,15 @@ class BubbleViewModel: Identifiable, ObservableObject{
     @Published var width: CGFloat
     @Published var height: CGFloat
     @Published var lifetime: TimeInterval = 0
+    let heightLitmited: CGFloat
     
-    init(height: CGFloat, width: CGFloat, x: CGFloat, y: CGFloat, color: Color, lifetime: TimeInterval){
+    init(height: CGFloat, width: CGFloat, x: CGFloat, y: CGFloat, color: Color, heightLitmited: CGFloat, lifetime: TimeInterval){
         self.height = height
         self.width = width
         self.color = color
         self.x = x
         self.y = y
+        self.heightLitmited = heightLitmited
         self.lifetime = lifetime
     }
     
--- RawUIRecord/Sources/WaveForm/WaveFormView.swift
+++ RawUIRecord/Sources/WaveForm/WaveFormView.swift
@@ -8,21 +8,35 @@
 import SwiftUI
 
 struct WaveFormView: View {
-
+    @State private var percent: CGFloat = 0.5
     var body: some View {
         ZStack {
             Color.appBackground.ignoresSafeArea()
             
-            WaveFormEffectView()
+            WaveFormEffectView(percent: $percent, waveFormConfig: .init())
         }
     }
 }
 
+struct WaveFormConfigProps {
+    var mainImage: String = "bottle"
+    var maskImage: String = "bottle"
+    
+    var waveFirstColor: Color = Color(red: 203/255, green: 225/255, blue: 1)
+    var waveSecondColor: Color = Color(red: 146/255, green: 192/255, blue: 1)
+    
+    var waveExtendWaveHeight: CGFloat = 20
+    var waveRollRatio: Double = 0.02
+    var waveFrequency: Double = 1.9
+    var waveSpeed: Double = 1.0
+
+    var bubbleConfig: BubbleConfigProps = .init()
+}
+
 struct WaveFormEffectView: View {
-    @State private var percent: CGFloat = 0.5
-    
-    private let waveRollRatio: Double = 0.02
-    
+    @Binding var percent: CGFloat
+    let waveFormConfig: WaveFormConfigProps
+
     var body: some View {
         VStack {
             Spacer()
@@ -41,13 +55,13 @@ struct WaveFormEffectView: View {
             
             Spacer()
             
-            Slider(value: $percent)
+            Slider(value: $percent, in: 0...1)
         }
     }
     
     @ViewBuilder
     private func waveBackgroundImage() -> some View {
-        Image("bottle")
+        Image(waveFormConfig.mainImage)
             .resizable()
             .renderingMode(.template)
             .aspectRatio(contentMode: .fit)
@@ -58,40 +72,40 @@ struct WaveFormEffectView: View {
     
     @ViewBuilder
     private func waveLayers(in size: CGSize) -> some View {
-        WaveLayerView(percent: $percent, waveRollRatio: waveRollRatio)
-        .overlay(
-            BubbleEffectView()
-                .frame(width: size.width, height: size.height * max(0, percent - waveRollRatio)),
-            alignment: .bottom
-        )
-        .overlay(
-            Text("\(percent * 100, specifier: "%.0f")%")
-                .foregroundStyle(.appForeground)
-                .font(.system(size: 30, weight: .black))
-                .offset(y: -((size.height / 2) * (max(0.1, percent) - waveRollRatio))),
-            alignment: .bottom
-        )
-        .mask {
-            Image("bottle")
-                .resizable()
-                .renderingMode(.template)
-                .aspectRatio(contentMode: .fit)
-                .scaleEffect(0.98)
-        }
+        WaveLayerView(percent: $percent, waveFormConfig: waveFormConfig)
+            .overlay(
+                BubbleEffectView(config: waveFormConfig.bubbleConfig)
+                    .frame(width: size.width, height: size.height * max(0, percent - waveFormConfig.waveRollRatio)), // Adjusted for default waveRollRatio
+                alignment: .bottom
+            )
+            .overlay(
+                Text("\(percent * 100, specifier: "%.0f")%")
+                    .foregroundColor(.blue)
+                    .font(.system(size: 30, weight: .black))
+                    .offset(y: -((size.height / 2) * (max(0.1, percent) - waveFormConfig.waveRollRatio))), // Adjusted for default waveRollRatio
+                alignment: .bottom
+            )
+            .mask {
+                Image(waveFormConfig.maskImage)
+                    .resizable()
+                    .renderingMode(.template)
+                    .aspectRatio(contentMode: .fit)
+                    .scaleEffect(0.98)
+            }
     }
 }
 
 private struct WaveLayerView: View {
     @Binding var percent: CGFloat
-    var waveRollRatio: Double
+    var waveFormConfig: WaveFormConfigProps
     
     @State private var waveOffset = Angle(degrees: 0)
     
     private func startWaveAnimation() {
         // need to delay state change a bit (until first layout/render finished)
         // this avoid effect to parent view
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            withAnimation(.linear(duration: 1.0).repeatForever(autoreverses: false)) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation(.linear(duration: waveFormConfig.waveSpeed).repeatForever(autoreverses: false)) {
                 self.waveOffset = Angle(degrees: 360)
             }
         }
@@ -102,20 +116,20 @@ private struct WaveLayerView: View {
             WaveShape(
                 offset: Angle(degrees: -waveOffset.degrees),
                 percent: percent,
-                waveRollRatio: waveRollRatio,
-                waveFrequency: 2.1,
+                waveRollRatio: waveFormConfig.waveRollRatio,
+                waveFrequency: waveFormConfig.waveFrequency + 0.2,
                 extendWaveHeight: 0
             )
-            .fill(Color(red: 203/255, green: 225/255, blue: 1))
+            .fill(waveFormConfig.waveFirstColor)
             
             WaveShape(
                 offset: Angle(degrees: waveOffset.degrees),
                 percent: percent,
-                waveRollRatio: waveRollRatio * 0.9,
-                waveFrequency: 1.9,
-                extendWaveHeight: 20
+                waveRollRatio: waveFormConfig.waveRollRatio * 0.9,
+                waveFrequency: waveFormConfig.waveFrequency,
+                extendWaveHeight: waveFormConfig.waveExtendWaveHeight
             )
-            .fill(Color(red: 146/255, green: 192/255, blue: 1))
+            .fill(waveFormConfig.waveSecondColor)
         }
         .onAppear(perform: startWaveAnimation)
     }
